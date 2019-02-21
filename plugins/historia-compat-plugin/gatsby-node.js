7acaa0a7fb2d35efcc05b874e641616388481929
Chitoku <odango@chitoku.jp>
2019-02-21 13:17:07 +0900

    Fix an issue where IE throws a syntax error

--- plugins/historia-compat-plugin/gatsby-node.js
+++ plugins/historia-compat-plugin/gatsby-node.js
@@ -11,7 +11,10 @@ exports.onCreateWebpackConfig = ({
       rules: [
         {
           test: /\.js$/,
-          include: path.resolve('node_modules/@weknow/gatsby-remark-twitter'),
+          include: [
+            path.resolve('node_modules/@weknow/gatsby-remark-twitter'),
+            path.resolve('node_modules/react-schemaorg'),
+          ],
           use: [
             loaders.js(),
           ],
