8e74ee4d0021b8bc5a1057aae2f9c8b60028da32
Chitoku <odango@chitoku.jp>
2019-02-22 01:55:44 +0900

    Fix an issue where IE throws an unsupported operation error

--- plugins/historia-compat-plugin/gatsby-browser.js
+++ plugins/historia-compat-plugin/gatsby-browser.js
@@ -2,6 +2,8 @@
 // https://www.npmjs.com/package/babel-preset-gatsby
 exports.onClientEntry = () => {
   require('core-js/fn/array/flat-map')
+  require('core-js/fn/array/from')
+  require('core-js/modules/es6.symbol')
   require('url-search-params-polyfill')
   require('whatwg-fetch')
 }
