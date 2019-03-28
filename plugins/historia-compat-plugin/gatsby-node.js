950662c615b8bfab034b33b7024de4517b5a0033
Chitoku <chitoku-k@users.noreply.github.com>
2019-03-29 02:00:30 +0900

    Fix compat plugin not to transpile react* packages (#143)
    Older implementation transpiled packages whose name start with react,
    therefore some of the related packages like `react-dom` are
    unintentionally included.

--- plugins/historia-compat-plugin/gatsby-node.js
+++ plugins/historia-compat-plugin/gatsby-node.js
@@ -12,9 +12,9 @@ exports.onCreateWebpackConfig = ({
         {
           test: /\.js$/,
           include: [
-            path.resolve('node_modules/@weknow/gatsby-remark-twitter'),
-            path.resolve('node_modules/react'),
-            path.resolve('node_modules/react-schemaorg'),
+            path.join(path.resolve('node_modules/@weknow/gatsby-remark-twitter'), '/'),
+            path.join(path.resolve('node_modules/react'), '/'),
+            path.join(path.resolve('node_modules/react-schemaorg'), '/'),
           ],
           use: [
             loaders.js(),
