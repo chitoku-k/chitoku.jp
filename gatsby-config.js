2d6851d17befc7cabaab5b946cccf8fb56ee6ec1
Chitoku <odango@chitoku.jp>
2019-05-17 22:41:58 +0900

    Remove deprecated option from gatsby-plugin-sharp
    Removed `sizeByPixelDensity`

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,7 +84,6 @@ module.exports = {
               linkImagesToOriginal: false,
               maxWidth: 768,
               quality: 100,
-              sizeByPixelDensity: true,
             },
           },
           { resolve: 'gatsby-remark-grid-tables' },
