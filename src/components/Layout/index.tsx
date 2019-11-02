c9be4a5b33cf2297759643ea6ba342cfb73a1f43
Chitoku <odango@chitoku.jp>
2019-11-02 16:33:38 +0900

    Use searchClient in InstantSearch

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,11 +1,11 @@
 import React, { FunctionComponent } from 'react'
 import { IntlProvider } from 'react-intl'
-import { InstantSearch } from 'react-instantsearch-dom'
 import styled from 'styled-components'
 
 import '../../styles/styles.scss'
 import styles from './styles'
 import messages from 'translations/ja.yml'
+import Search from 'components/Search'
 
 export const media = styles
 
@@ -20,14 +20,11 @@ const Layout: FunctionComponent = ({
   children,
 }) => (
   <IntlProvider locale="ja" messages={messages}>
-    <InstantSearch
-      apiKey={process.env.GATSBY_ALGOLIA_APIKEY_SEARCH_ONLY as string}
-      appId={process.env.GATSBY_ALGOLIA_APPID as string}
-      indexName={process.env.GATSBY_ALGOLIA_INDEXNAME as string}>
+    <Search>
       <Wrapper>
         {children}
       </Wrapper>
-    </InstantSearch>
+    </Search>
   </IntlProvider>
 )
 
