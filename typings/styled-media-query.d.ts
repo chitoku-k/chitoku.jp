95433db36489552791d8a58c2bb10777de8c8563
Chitoku <odango@chitoku.jp>
2019-11-10 04:31:27 +0900

    Fix typings

--- typings/styled-media-query.d.ts
+++ typings/styled-media-query.d.ts
@@ -1,32 +1,30 @@
 import { css } from 'styled-components'
 
-declare module 'styled-media-query' {
-  interface Media<T extends Breakpoints> {
-    lessThan(breakpoint: keyof T): css
-    lessThan(size: string): css
+interface Media<T extends Breakpoints> {
+  lessThan(breakpoint: keyof T): typeof css
+  lessThan(size: string): typeof css
 
-    greaterThan(breakpoint: keyof T): css
-    greaterThan(size: string): css
+  greaterThan(breakpoint: keyof T): typeof css
+  greaterThan(size: string): typeof css
 
-    between(from: keyof T, to: keyof T): css
-    between(from: string, to: string): css
-  }
-
-  interface DefaultBreakpoints extends Breakpoints {
-    huge: string
-    large: string
-    medium: string
-    small: string
-  }
-
-  interface Breakpoints {
-    [key: string]: string
-  }
-
-  export function generateMedia<T extends Breakpoints>(breakpoints: T): Media<T>
-  export function pxToRem(breakpoints: Breakpoints): Breakpoints
-  export function pxToEm(breakpoints: Breakpoints, ratio?: number): Breakpoints
-
-  const media: Media<DefaultBreakpoints>
-  export default media
+  between(from: keyof T, to: keyof T): typeof css
+  between(from: string, to: string): typeof css
 }
+
+interface DefaultBreakpoints extends Breakpoints {
+  huge: string
+  large: string
+  medium: string
+  small: string
+}
+
+interface Breakpoints {
+  [key: string]: string
+}
+
+export function generateMedia<T extends Breakpoints>(breakpoints: T): Media<T>
+export function pxToRem(breakpoints: Breakpoints): Breakpoints
+export function pxToEm(breakpoints: Breakpoints, ratio?: number): Breakpoints
+
+declare const media: Media<DefaultBreakpoints>
+export default media
