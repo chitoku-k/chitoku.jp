b5c1acd7e3f360af6673e93f5a91c08e2cba1913
Chitoku <chitoku-k@users.noreply.github.com>
2020-05-16 23:45:14 +0900

    Use babel-preset-gatsby target for compatibility (#1476)
    * Use babel-preset-gatsby target for compatibility
    
    * Regenerate translations

--- plugins/historia-compat-plugin/gatsby-browser.js
+++ plugins/historia-compat-plugin/gatsby-browser.js
@@ -1,11 +1,6 @@
 'use strict'
 
 /* eslint-disable global-require */
-// https://www.gatsbyjs.org/docs/browser-support/
-// https://www.npmjs.com/package/babel-preset-gatsby
 exports.onClientEntry = () => {
-  require('core-js/fn/array/flat-map')
-  require('core-js/fn/array/from')
-  require('core-js/modules/es6.symbol')
   require('whatwg-fetch')
 }
