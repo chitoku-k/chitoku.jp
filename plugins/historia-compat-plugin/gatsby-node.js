9f52cd1b707cba6ab72f3c607dc5b37fdfcfeb8e
Chitoku <odango@chitoku.jp>
2020-05-16 15:44:42 +0900

    Update historia-compat-plugin

--- plugins/historia-compat-plugin/gatsby-node.js
+++ plugins/historia-compat-plugin/gatsby-node.js
@@ -12,13 +12,14 @@ exports.onCreateWebpackConfig = ({
     module: {
       rules: [
         {
-          test: /\.m?js$/u,
+          test: /\.js$/u,
           include: [
             path.join(path.resolve('node_modules/@pmmmwh/react-refresh-webpack-plugin'), '/'),
             path.join(path.resolve('node_modules/@weknow/gatsby-remark-twitter'), '/'),
+            path.join(path.resolve('node_modules/query-string'), '/'),
             path.join(path.resolve('node_modules/react'), '/'),
             path.join(path.resolve('node_modules/react-intl'), '/'),
-            path.join(path.resolve('node_modules/react-schemaorg'), '/'),
+            path.join(path.resolve('node_modules/split-on-first'), '/'),
           ],
           use: [
             loaders.js(),
