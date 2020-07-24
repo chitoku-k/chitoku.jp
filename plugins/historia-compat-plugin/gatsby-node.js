e961f3c48f4a0c267424639507105c0cc948d101
Chitoku <odango@chitoku.jp>
2020-07-25 01:18:17 +0900

    Fix compatibility issues in IE

--- plugins/historia-compat-plugin/gatsby-node.js
+++ plugins/historia-compat-plugin/gatsby-node.js
@@ -14,8 +14,10 @@ exports.onCreateWebpackConfig = ({
         {
           test: /\.js$/u,
           include: [
+            path.join(path.resolve('node_modules/$virtual'), '/'),
             path.join(path.resolve('node_modules/@pmmmwh/react-refresh-webpack-plugin'), '/'),
             path.join(path.resolve('node_modules/@weknow/gatsby-remark-twitter'), '/'),
+            path.join(path.resolve('node_modules/debug'), '/'),
             path.join(path.resolve('node_modules/query-string'), '/'),
             path.join(path.resolve('node_modules/react'), '/'),
             path.join(path.resolve('node_modules/split-on-first'), '/'),
