69e4dddebd79bb37342fe613f3c9602f0629cc02
Chitoku <odango@chitoku.jp>
2021-02-22 06:12:44 +0900

    Fix a compatibility issue in IE

--- plugins/historia-compat-plugin/gatsby-browser.ts
+++ plugins/historia-compat-plugin/gatsby-browser.ts
@@ -3,6 +3,7 @@ import type { GatsbyBrowser } from 'gatsby'
 /* eslint-disable @typescript-eslint/no-require-imports */
 /* eslint-disable global-require */
 export const onClientEntry: GatsbyBrowser['onClientEntry'] = () => {
+  require('@webcomponents/shadydom')
   require('core-js/modules/web.dom-collections.for-each')
   require('element-matches')
   require('whatwg-fetch')
