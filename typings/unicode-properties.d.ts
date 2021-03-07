cdfca5a5b6b2bc1ecbac829a44075fa9f56d6213
Chitoku <odango@chitoku.jp>
2021-03-08 02:11:43 +0900

    Discard newlines between EastAsianWidth characters
    Mimics the implementation in Firefox
    
    See: https://searchfox.org/mozilla-central/rev/ca22dc04/layout/generic/nsTextFrameUtils.cpp#115-118

---
+++ typings/unicode-properties.d.ts
@@ -0,0 +1,14 @@
+export function getCategory(codePoint: number): string | undefined
+export function getCombiningClass(codePoint: number): string | undefined
+export function getScript(codePoint: number): string | undefined
+export function getEastAsianWidth(codePoint: number): string | undefined
+export function getNumericValue(codePoint: number): number | null
+export function isAlphabetic(codePoint: number): boolean
+export function isDigit(codePoint: number): boolean
+export function isPunctuation(codePoint: number): boolean
+export function isLowerCase(codePoint: number): boolean
+export function isUpperCase(codePoint: number): boolean
+export function isTitleCase(codePoint: number): boolean
+export function isWhiteSpace(codePoint: number): boolean
+export function isBaseForm(codePoint: number): boolean
+export function isMark(codePoint: number): boolean
