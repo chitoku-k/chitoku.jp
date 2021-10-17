f0d6b445cf098eb496e963eadf7c5d210d49ad3a
Chitoku <odango@chitoku.jp>
2021-10-18 01:54:32 +0900

    Add Storybook

---
+++ src/components/Header/index.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { LocationProvider, createHistory, createMemorySource } from '@reach/router'
+import type { Meta, Story } from '@storybook/react'
+
+import '../../styles/styles.scss'
+import Header from 'components/Header'
+
+const meta: Meta = {
+  component: Header,
+  title: 'Components/Header',
+}
+
+const history = createHistory(createMemorySource('/'))
+
+const Template: Story = () => (
+  <LocationProvider history={history}>
+    <Header />
+  </LocationProvider>
+)
+
+export default meta
+export const Default = Template.bind({})
