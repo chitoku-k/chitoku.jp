cfa5bb1c70d72abb6a280cdb654cdb7a6f5e399a
Chitoku <odango@chitoku.jp>
2021-10-23 02:45:38 +0900

    Add story for Navbar

---
+++ src/components/Navbar/index.stories.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { IntlProvider } from 'react-intl'
+import { LocationProvider, createHistory, createMemorySource } from '@reach/router'
+import type { Meta, Story } from '@storybook/react'
+
+import '../../styles/styles.scss'
+import messages from 'translations/ja.yml'
+import Navbar from 'components/Navbar'
+import Search from 'components/Search'
+
+const meta: Meta = {
+  component: Navbar,
+  title: 'Components/Navbar',
+  parameters: {
+    layout: 'fullscreen',
+  },
+}
+
+const history = createHistory(createMemorySource('/'))
+
+const Template: Story = () => (
+  <Search>
+    <IntlProvider locale="ja" messages={messages}>
+      <LocationProvider history={history}>
+        <Navbar />
+      </LocationProvider>
+    </IntlProvider>
+  </Search>
+)
+
+export default meta
+export const Default = Template
