eb4452ad797228a20969c5ae2f3d913e06733a16
Chitoku <odango@chitoku.jp>
2021-10-23 02:46:14 +0900

    Add story for TwitterTweet

---
+++ src/components/TwitterTweet/index.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { LocationProvider, createHistory, createMemorySource } from '@reach/router'
+import type { Meta, Story } from '@storybook/react'
+
+import '../../styles/styles.scss'
+import type { TwitterTweetProps } from 'components/TwitterTweet'
+import TwitterTweet from 'components/TwitterTweet'
+
+const meta: Meta = {
+  component: TwitterTweet,
+  title: 'Components/TwitterTweet',
+  parameters: {
+    backgrounds: {
+      default: 'container',
+    },
+  },
+}
+
+const history = createHistory(createMemorySource('/'))
+
+const Template: Story<TwitterTweetProps> = props => (
+  <LocationProvider history={history}>
+    <TwitterTweet {...props} />
+  </LocationProvider>
+)
+
+export default meta
+export const Default = Template
+Default.args = {
+  id: '1097194006700675073',
+}
