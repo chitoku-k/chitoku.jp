b78c540dd0d13dee35693290a6743026c958088f
Chitoku <odango@chitoku.jp>
2021-10-23 02:50:28 +0900

    Split Pagination and SimplePagination components to different files

---
+++ src/components/Pagination/simple.tsx
@@ -0,0 +1,47 @@
+import type { ComponentPropsWithoutRef, FunctionComponent, ReactNode } from 'react'
+import { Pagination as BootstrapPagination } from 'react-bootstrap'
+import type { BsPrefixProps } from 'react-bootstrap/helpers'
+import { useIntl } from 'react-intl'
+
+import messages from './messages'
+import { PaginationItem } from './'
+import * as styles from './styles.module.scss'
+
+export const SimplePagination: FunctionComponent<BsPrefixProps<'ul'> & ComponentPropsWithoutRef<'ul'> & SimplePaginationProps> = ({
+  prev,
+  next,
+  ...rest
+}) => {
+  const { formatMessage } = useIntl()
+
+  return (
+    <BootstrapPagination className={styles.simplePagination} {...rest}>
+      <PaginationItem className={styles.simplePaginationItem} direction="prev" visible={Boolean(prev)} href={prev ? prev.to : '#'}>
+        {prev ? (
+          <>
+            <span className={styles.icon}>{formatMessage(messages.prev)}</span>
+            <span>{prev.title}</span>
+          </>
+        ) : null}
+      </PaginationItem>
+      <PaginationItem className={styles.simplePaginationItem} direction="next" visible={Boolean(next)} href={next ? next.to : '#'}>
+        {next ? (
+          <>
+            <span>{next.title}</span>
+            <span className={styles.icon}>{formatMessage(messages.next)}</span>
+          </>
+        ) : null}
+      </PaginationItem>
+    </BootstrapPagination>
+  )
+}
+
+export interface SimplePaginationItem {
+  title: ReactNode
+  to: string
+}
+
+export interface SimplePaginationProps {
+  prev?: SimplePaginationItem | null
+  next?: SimplePaginationItem | null
+}
