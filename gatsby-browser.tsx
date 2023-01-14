7f8f57dd851d9e582d3e1f0b47e406b5246f43b2
Chitoku <chitoku-k@users.noreply.github.com>
2023-01-15 05:40:36 +0900

    Refactor gatsby-{browser,ssr}.tsx by adding gatsby-shared.tsx (#4319)

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,50 +1,9 @@
-import { isValidElement } from 'react'
-import type { FunctionComponent, PropsWithChildren } from 'react'
 import type { GatsbyBrowser } from 'gatsby'
-import { BaseContext } from '@gatsbyjs/reach-router'
 
 import Prism from 'prismjs'
-import { IntlProvider } from 'react-intl'
 
-import messages from 'translations/ja.yml'
-
-import Layout from 'components/Layout'
-import Container from 'components/Container'
-import Header from 'components/Header'
-import Navbar from 'components/Navbar'
-import Footer from 'components/Footer'
-import { SearchProvider } from 'components/Search'
-
-const Wrapper: FunctionComponent<PropsWithChildren> = ({ children }) => isValidElement(children) ? children : null
+export { wrapPageElement, wrapRootElement } from './gatsby-shared'
 
 export const onClientEntry: GatsbyBrowser['onClientEntry'] = () => {
   Prism.manual = true
 }
-
-export const wrapPageElement: GatsbyBrowser<unknown, Context>['wrapPageElement'] = ({
-  element,
-  props: {
-    pageContext: {
-      sidebar,
-    },
-  },
-}) => (
-  <Layout>
-    <Header />
-    <Navbar />
-    <Container sidebar={sidebar !== false}>
-      {element}
-    </Container>
-    <Footer />
-  </Layout>
-)
-
-export const wrapRootElement: GatsbyBrowser['wrapRootElement'] = ({ element }) => (
-  <IntlProvider locale="ja" messages={messages}>
-    <SearchProvider>
-      <BaseContext.Provider value={{ basepath: '/', component: Wrapper, primary: false }}>
-        {element}
-      </BaseContext.Provider>
-    </SearchProvider>
-  </IntlProvider>
-)
--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,50 +1,9 @@
-import { isValidElement } from 'react'
-import type { FunctionComponent, PropsWithChildren } from 'react'
 import type { GatsbySSR } from 'gatsby'
-import { BaseContext } from '@gatsbyjs/reach-router'
-import { IntlProvider } from 'react-intl'
 
-import messages from 'translations/ja.yml'
-
-import Layout from 'components/Layout'
-import Container from 'components/Container'
-import Header from 'components/Header'
-import Navbar from 'components/Navbar'
-import Footer from 'components/Footer'
-import { SearchProvider } from 'components/Search'
-
-const Wrapper: FunctionComponent<PropsWithChildren> = ({ children }) => isValidElement(children) ? children : null
+export { wrapPageElement, wrapRootElement } from './gatsby-shared'
 
 export const onRenderBody: GatsbySSR['onRenderBody'] = ({ setHtmlAttributes }) => {
   setHtmlAttributes({
     lang: 'ja',
   })
 }
-
-export const wrapPageElement: GatsbySSR<unknown, Context>['wrapPageElement'] = ({
-  element,
-  props: {
-    pageContext: {
-      sidebar,
-    },
-  },
-}) => (
-  <Layout>
-    <Header />
-    <Navbar />
-    <Container sidebar={sidebar !== false}>
-      {element}
-    </Container>
-    <Footer />
-  </Layout>
-)
-
-export const wrapRootElement: GatsbySSR['wrapRootElement'] = ({ element }) => (
-  <IntlProvider locale="ja" messages={messages}>
-    <SearchProvider>
-      <BaseContext.Provider value={{ basepath: '/', component: Wrapper, primary: false }}>
-        {element}
-      </BaseContext.Provider>
-    </SearchProvider>
-  </IntlProvider>
-)
