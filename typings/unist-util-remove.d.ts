0128b5831b897564e170dd018b853a1853109131
Chitoku <odango@chitoku.jp>
2023-01-17 02:43:08 +0900

    Remove unused dependencies

--- typings/unist-util-remove.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Node } from 'unist'
-import type { Test } from 'unist-util-is'
-
-declare function remove<T extends Node>(
-  tree: T,
-  test?: Test<T>,
-): Node | null
-
-export = remove
